a0a25f0820daf74e03247fba55596efb84c888c0
JaQuan Taylor <jkaet@protonmail.com>
2020-04-06 04:15:41 -0400

    added equal and clear functionality

--- main.jl
+++ main.jl
@@ -3,7 +3,17 @@ using Gtk
 function button_connect(b)
   signal_connect(b, "clicked") do widget
     set_gtk_property!(ent,:text,string(get_gtk_property(ent,:text,String),get_gtk_property(widget,:label,String)))
-    println("\"", get_gtk_property(widget,:label,String), "\" was clicked!")
+  end
+end
+function clear_button_connect(b)
+  signal_connect(b, "clicked") do widget
+    set_gtk_property!(ent,:text,"")
+  end
+end
+function equal_button_connect(b)
+  signal_connect(b, "clicked") do widget
+    answer= eval(Meta.parse(get_gtk_property(ent,:text,String)))
+    set_gtk_property!(ent,:text,"$answer")
   end
 end
 win = GtkWindow("A new window")
@@ -17,10 +27,13 @@ for x= 1:3,y = 1:3
 end
 g[1:3,1] = ent
 operations = [GtkButton("+"),GtkButton("-"),GtkButton("*"),GtkButton("/")]
+functional =[GtkButton("C"),GtkButton("=")]
 g[1,5],g[2,5] ,g[3,5],g[1,6]  = operations[1],operations[2],operations[3],operations[4]
+g[2,6],g[3,6]= functional[1],functional[2]
 numberedButtons = numberedButtons .|> button_connect
 operations = operations .|> button_connect
-
+clear_button_connect(functional[1])
+equal_button_connect(functional[2])
 set_gtk_property!(g, :column_homogeneous, true)
 set_gtk_property!(g, :column_spacing, 1)  # introduce a 15-pixel gap between columns
 push!(win, g)
